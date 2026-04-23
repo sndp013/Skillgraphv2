280d3fd79d40f3e46601ad711c5891c5999f2673
Sandeep Jadhav <sndp013@gmail.com>
2026-04-23 21:59:27 +0530

    fix: restore missing ThemeToggle import in Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { ThemeToggle } from './ThemeToggle';
 import { useProfile } from '@/context/ProfileContext';
 
 export default function Navbar() {
